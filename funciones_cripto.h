425d91ae11fc92d21e620ffd322c6a5921265830
DESKTOP-5MOFOR3\byron <hbcarvaj@espol.edu.ec>
2017-10-28 21:33:45 -0500

    main

---
+++ funciones_cripto.h
@@ -0,0 +1,6 @@
+char* cifrado_ciclico(char* nombre, int avance);
+char* charToMorse(char letra);
+void stringToMorse(char* Palabra);
+char* func(char* nombre);
+char* cifrado_aciclico(char* nombre);
+
